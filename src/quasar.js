e20f2e3d5a51b3931e2428ada41e56649bf5796e
Isabela Karkache <isabelakarkache@gmail.com>
2021-04-24 00:04:35 -0300

    Address Finder - Project init

---
+++ src/quasar.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+import './styles/quasar.sass'
+import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import lang from 'quasar/lang/pt-br.js'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  },
+  lang: lang,
+  iconSet: iconSet
+ })
